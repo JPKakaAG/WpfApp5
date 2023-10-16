978613473e1fdc73a194dd8780ff8f920ab4e205
JPKakaAG <Gjopka@yandex.ru>
2023-10-16 11:14:10 +0300

    практическая 5

--- WpfApp5/Pair.cs
+++ WpfApp5/Pair.cs
@@ -11,12 +11,23 @@ namespace WpfApp5
         public int First { get; set; }
         public int Second { get; set; }
 
+        /// <summary>
+        /// Конструктор, принимающий значения для обоих чисел в паре
+        /// </summary>
+        /// <param name="first"> первое число</param>
+        /// <param name="second"> второе число</param>
+
         public Pair(int first, int second)
         {
             First = first;
             Second = second;
         }
 
+        /// <summary>
+        /// Метод для умножения пары на другую пару чисел
+        /// </summary>
+        /// <param name="other"> числа из второй пары</param>
+        /// <returns>возвращает результат перемножения пар</returns>
         public Pair Multiply(Pair other)
         {
             int multipliedFirst = First * other.First;
@@ -24,6 +35,9 @@ namespace WpfApp5
             return new Pair(multipliedFirst, multipliedSecond);
         }
 
+        /// <summary>
+        /// Метод для удвоения значений пары
+        /// </summary>
         public void Double()
         {
             First *= 2;
